d21f35f8b7eb777527336869ae286dbb0d50dc9e
ThaminduChankana <thaminduchankana@gmail.com>
2023-05-02 09:58:15 +0530

    created site reducers

---
+++ src/reducers/siteManagementReducers/siteReducers.js
@@ -0,0 +1,104 @@
+import {
+	SITES_LIST_FOR_CUSTOMER_REQUEST,
+	SITES_LIST_FOR_CUSTOMER_SUCCESS,
+	SITES_LIST_FOR_CUSTOMER_FAIL,
+	SITES_LIST_FOR_EACH_LOCATION_REQUEST,
+	SITES_LIST_FOR_EACH_LOCATION_SUCCESS,
+	SITES_LIST_FOR_EACH_LOCATION_FAIL,
+	SITES_CREATE_REQUEST,
+	SITES_CREATE_SUCCESS,
+	SITES_CREATE_FAIL,
+	SITES_LIST_FOR_ADMIN_REQUEST,
+	SITES_LIST_FOR_ADMIN_SUCCESS,
+	SITES_LIST_FOR_ADMIN_FAIL,
+	SITES_UPDATE_BY_ADMIN_REQUEST,
+	SITES_UPDATE_BY_ADMIN_SUCCESS,
+	SITES_UPDATE_BY_ADMIN_FAIL,
+	SITES_DELETE_BY_ADMIN_REQUEST,
+	SITES_DELETE_BY_ADMIN_SUCCESS,
+	SITES_DELETE_BY_ADMIN_FAIL,
+} from "../../constants/siteManagementConstants/siteConstants";
+
+export const customerSiteListReducer = (state = { products: [] }, action) => {
+	switch (action.type) {
+		case SITES_LIST_FOR_CUSTOMER_REQUEST:
+			return { loading: true };
+		case SITES_LIST_FOR_CUSTOMER_SUCCESS:
+			return { loading: false, products: action.payload };
+		case SITES_LIST_FOR_CUSTOMER_FAIL:
+			return { loading: false, error: action.payload };
+
+		default:
+			return state;
+	}
+}
+
+export const adminSiteListReducer = (state = { products: [] }, action) => {
+	switch (action.type) {
+		case SITES_LIST_FOR_ADMIN_REQUEST:
+			return { loading: true };
+		case SITES_LIST_FOR_ADMIN_SUCCESS:
+			return { loading: false, products: action.payload };
+		case SITES_LIST_FOR_ADMIN_FAIL:
+			return { loading: false, error: action.payload };
+
+		default:
+			return state;
+	}
+}
+
+export const locationSiteListReducer = (state = { products: [] }, action) => {
+	switch (action.type) {
+		case SITES_LIST_FOR_EACH_LOCATION_REQUEST:
+			return { loading: true };
+		case SITES_LIST_FOR_EACH_LOCATION_SUCCESS:
+			return { loading: false, products: action.payload };
+		case SITES_LIST_FOR_EACH_LOCATION_FAIL:
+			return { loading: false, error: action.payload };
+
+		default:
+			return state;
+	}
+}
+
+export const siteCreateReducer = (state = {}, action) => {
+	switch (action.type) {
+		case SITES_CREATE_REQUEST:
+			return { loading: true };
+		case SITES_CREATE_SUCCESS:
+			return { loading: false, success: true };
+		case SITES_CREATE_FAIL:
+			return { loading: false, error: action.payload };
+
+		default:
+			return state;
+	}
+};
+
+export const siteUpdateByAdminReducer = (state = {}, action) => {
+	switch (action.type) {
+		case SITES_UPDATE_BY_ADMIN_REQUEST:
+			return { loading: true };
+		case SITES_UPDATE_BY_ADMIN_SUCCESS:
+			return { loading: false, success: true };
+		case SITES_UPDATE_BY_ADMIN_FAIL:
+			return { loading: false, error: action.payload, success: false };
+
+		default:
+			return state;
+	}
+};
+
+export const siteDeleteByAdminReducer = (state = {}, action) => {
+	switch (action.type) {
+		case SITES_DELETE_BY_ADMIN_REQUEST:
+			return { loading: true };
+		case SITES_DELETE_BY_ADMIN_SUCCESS:
+			return { loading: false, success: true };
+		case SITES_DELETE_BY_ADMIN_FAIL:
+			return { loading: false, error: action.payload, success: false };
+
+		default:
+			return state;
+	}
+};
