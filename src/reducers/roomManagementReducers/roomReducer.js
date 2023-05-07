01e32812f45a77d6d0597dd2db281625d66bad03
ChathurikaThathsarani <it20603168@my.sliit.lk>
2023-05-07 10:27:15 +0530

    Create reducers for room

---
+++ src/reducers/roomManagementReducers/roomReducer.js
@@ -0,0 +1,87 @@
+import {
+	ROOM_LIST_ADMIN_REQUEST,
+	ROOM_LIST_ADMIN_SUCCESS,
+	ROOM_LIST_ADMIN_FAIL,
+	ROOM_CREATE_ADMIN_REQUEST,
+	ROOM_CREATE_ADMIN_SUCCESS,
+	ROOM_CREATE_ADMIN_FAIL,
+	ROOM_UPDATE_ADMIN_REQUEST,
+	ROOM_UPDATE_ADMIN_SUCCESS,
+	ROOM_UPDATE_ADMIN_FAIL,
+	ROOM_DELETE_ADMIN_REQUEST,
+	ROOM_DELETE_ADMIN_SUCCESS,
+	ROOM_DELETE_ADMIN_FAIL,
+	ROOM_LIST_CUSTOMER_REQUEST,
+	ROOM_LIST_CUSTOMER_SUCCESS,
+	ROOM_LIST_CUSTOMER_FAIL,
+} from "../../constants/roomManagementConstants/roomConstant";
+
+export const roomListAdminReducer = (state = { adminRooms: [] }, action) => {
+	switch (action.type) {
+		case ROOM_LIST_ADMIN_REQUEST:
+			return { loading: true };
+		case ROOM_LIST_ADMIN_SUCCESS:
+			return { loading: false, adminRooms: action.payload };
+		case ROOM_LIST_ADMIN_FAIL:
+			return { loading: false, error: action.payload };
+
+		default:
+			return state;
+	}
+};
+
+export const roomListCustomerReducer = (state = { customerRooms: [] }, action) => {
+	switch (action.type) {
+		case ROOM_LIST_CUSTOMER_REQUEST:
+			return { loading: true };
+		case ROOM_LIST_CUSTOMER_SUCCESS:
+			return { loading: false, customerRooms: action.payload };
+		case ROOM_LIST_CUSTOMER_FAIL:
+			return { loading: false, error: action.payload };
+
+		default:
+			return state;
+	}
+};
+
+export const roomCreateReducer = (state = {}, action) => {
+	switch (action.type) {
+		case ROOM_CREATE_ADMIN_REQUEST:
+			return { loading: true };
+		case ROOM_CREATE_ADMIN_SUCCESS:
+			return { loading: false, success: true };
+		case ROOM_CREATE_ADMIN_FAIL:
+			return { loading: false, error: action.payload };
+
+		default:
+			return state;
+	}
+};
+
+export const roomUpdateReducer = (state = {}, action) => {
+	switch (action.type) {
+		case ROOM_UPDATE_ADMIN_REQUEST:
+			return { loading: true };
+		case ROOM_UPDATE_ADMIN_SUCCESS:
+			return { loading: false, success: true };
+		case ROOM_UPDATE_ADMIN_FAIL:
+			return { loading: false, error: action.payload, success: false };
+
+		default:
+			return state;
+	}
+};
+
+export const roomDeleteReducer = (state = {}, action) => {
+	switch (action.type) {
+		case ROOM_DELETE_ADMIN_REQUEST:
+			return { loading: true };
+		case ROOM_DELETE_ADMIN_SUCCESS:
+			return { loading: false, success: true };
+		case ROOM_DELETE_ADMIN_FAIL:
+			return { loading: false, error: action.payload, success: false };
+
+		default:
+			return state;
+	}
+};
